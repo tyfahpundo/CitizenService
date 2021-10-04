44f8a129f60bd93fd19cc523b31d3e93675cea29
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-04 14:34:08 +0200

    Initial Commit

---
+++ src/main/java/zw/co/afrosoft/citizenservice/CitizenServiceApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.citizenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitizenServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zw/co/afrosoft/citizenservice/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.citizenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
